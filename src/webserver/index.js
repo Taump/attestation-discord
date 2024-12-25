bce3abf97bab1b11d4d07431a044c9c52f107bf2
Taump <taump@yandex.ru>
2024-12-25 18:29:56 +0400

    feat: update Discord authentication route to exclude callback from device address parameter

--- src/webserver/index.js
+++ src/webserver/index.js
@@ -1,11 +1,11 @@
 const conf = require('ocore/conf');
 const { webserver: fastifyInstance, utils } = require("attestation-kit");
 
-const authController = require('./controllers/authCallbackController');
+const authController = require('./controllers/authController');
 const authCallbackController = require('./controllers/authCallbackController');
 
 module.exports = async () => {
-    fastifyInstance.get('/auth/discord/:deviceAddress', authController);
+    fastifyInstance.get('/auth/discord/:deviceAddress(^(?!callback$).+)', authController);
     fastifyInstance.get('/auth/discord/callback', authCallbackController);
 
     await fastifyInstance.listen({ port: conf.webserverPort, host: '0.0.0.0' });
