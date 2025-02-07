5e61f0425338bb7d6d6c32172b67b81645f0e6f5
tony <tonyofbyteball@users.noreply.github.com>
2025-02-07 15:08:10 +0700

    daemon name

--- check_daemon.js
+++ check_daemon.js
@@ -2,5 +2,5 @@
 "use strict";
 var check_daemon = require('ocore/check_daemon.js');
 
-check_daemon.checkDaemonAndNotify('yarn start discord');
+check_daemon.checkDaemonAndNotify('node src/index.js discord');
 
